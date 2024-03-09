0c234948cf81f19394250633d12c3cf697d8b3b6
Ginku53 <158774250+Ginku53@users.noreply.github.com>
2024-03-09 17:02:56 -0300

    Aula de banco

---
+++ Banco.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banco
+
+{
+    
+    int ContasBancarias = new ContasBancarias[]
+
+    void CriarContaBancaria()
+    {
+
+    }
+
+    void EncontrarContaBancariaPorNome()
+    {
+
+    }
+
+    void EncontrarContaBancariaPorNumero()
+    {
+
+    }
+
+    int TransacaoDeContas()
+    {
+
+    }
+}
